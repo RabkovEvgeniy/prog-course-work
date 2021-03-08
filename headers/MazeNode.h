4acedada27984b35a530e4c067e8043d8463ecf2
Евгений <rabkov_evgeniy@mail.ru>
2021-03-09 01:16:49 +0700

    Added maze node class definition

---
+++ headers/MazeNode.h
@@ -0,0 +1,12 @@
+
+//Развилка, поворот, узел на который опираются пути лабиринта.
+class MazeNode {
+    int x;
+    int y;
+
+public:
+    MazeNode(int x, int y);
+
+    int get_x();
+    int get_y();
+};
