5bded770db182bc6a4e1887dcee38eac1a8ed095
Евгений <rabkov_evgeniy@mail.ru>
2021-03-09 02:03:09 +0700

    Added header guard to the maze node class header

--- headers/MazeNode.h
+++ headers/MazeNode.h
@@ -1,3 +1,5 @@
+#ifndef MAZE_NODE
+#define MAZE_NODE
 
 //Развилка, поворот, узел на который опираются пути лабиринта.
 class MazeNode {
@@ -10,3 +12,5 @@ public:
     int get_x();
     int get_y();
 };
+
+#endif
